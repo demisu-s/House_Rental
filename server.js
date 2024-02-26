13640db5d8858df3243290a7234db2c4918cbc55
demisu-s <damisusefu@gmail.com>
2024-02-26 14:47:25 +0300

    folder structure

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const connectDB=require("./config/db")
+const app = express();
+require('dotenv').config();
+connectDB()
+
+const port = process.env.PORT || 4000;
+app.use(express.json())
+
+//routes here
+
+
+
+//starting server
+app.listen(port, (err) => {
+    if (err) {
+        console.log(`Error to start server`);
+    } else {
+        console.log(`Server is listening on port ${port}`);
+    }
+});
