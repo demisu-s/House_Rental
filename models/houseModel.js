d9c11c36cdd220b24b7922c83f5b48e65e842a8a
Betelhem Yemane <betelhemyemanezoom@gmail.com>
2024-03-02 12:28:30 -0800

    define the neccessary schema

---
+++ models/houseModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const houseSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    square_feet: {
+        type: Number,
+    },
+    bedrooms: {
+        type: Number,
+    },
+    bathrooms: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    property_type: {
+        type: String
+    },
+    images: {
+        type: Buffer
+    },
+    tenant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tenant'
+    },
+    broker: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Broker'
+    },
+},
+{
+    Timestamps: true,
+}
+)
+
+module.exports = mongoose.model('House', houseSchema)
