5ffdff0cc18222624519419edc1502c3ef73e321
Betelhem Yemane <betelhemyemanezoom@gmail.com>
2024-03-12 15:41:37 -0700

    fix get house by id

--- controllers/houseController.js
+++ controllers/houseController.js
@@ -15,7 +15,8 @@ exports.getHouseById = async (req,res) => {
         const house = await House.findById(req.params.id)
         if(!house)
             return res.status(404).json({message: 'House not found'})
-        res.status(500).json({message: err.message})
+        else
+            res.status(200).json(house)
     }
     catch(err) {
         res.status(500).json({message: err.message})
