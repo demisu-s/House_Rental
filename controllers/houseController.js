a53cc47ead872cde5dcb386afda783371f911e31
Betelhem Yemane <betelhemyemanezoom@gmail.com>
2024-03-12 15:28:46 -0700

    Revert "remove the new keword revert"
    This reverts commit 90d2c8eaa796c8eee3de728c97607d358be67e60.

--- controllers/houseController.js
+++ controllers/houseController.js
@@ -15,8 +15,7 @@ exports.getHouseById = async (req,res) => {
         const house = await House.findById(req.params.id)
         if(!house)
             return res.status(404).json({message: 'House not found'})
-        else
-            res.status(200).json(house)
+        res.status(500).json({message: err.message})
     }
     catch(err) {
         res.status(500).json({message: err.message})
