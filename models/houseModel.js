4956bd2677d94866a21fb8361e9e8d592634e567
BeamlakTesfahun <111596136+BeamlakTesfahun@users.noreply.github.com>
2024-03-17 20:45:33 +0300

    updated house model

--- models/houseModel.js
+++ models/houseModel.js
@@ -1,1 +1,42 @@
 
+const HouseSchema = new mongoose.Schema(
+  {
+    // need to add some taskss
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    landlord: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Reference to the user model
+      //required: true,
+    },
+    broker: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Reference to the user model
+      //required: true,
+    },
+    status: {
+      type: String,
+      enum: ["available", "rented", "unavailable"],
+      default: "available",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("House", HouseSchema);
